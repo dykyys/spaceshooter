b380b7594fcec3fa2a594a5d271e0ed24de1142b
Sergii <dykyy.s@gmail.com>
2024-11-26 12:12:18 +0200

    add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useRef } from 'react';
+import { Application } from 'pixi.js';
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const start = async () => {
+      const app = new Application();
+      await app.init({
+        width: 1280,
+        height: 720,
+        backgroundColor: 0x000000,
+      });
+
+      if (canvasRef.current) {
+        canvasRef.current.appendChild(app.canvas);
+      }
+    };
+
+    start();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div ref={canvasRef} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
